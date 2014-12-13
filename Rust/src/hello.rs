954687e9337e34445f5f385e4bfc934524474008
Mark Vasilkov <mvasilkov@gmail.com>
2014-12-14 00:05:47 +0200

    Changes

--- Rust/src/hello.rs
+++ Rust/src/hello.rs
@@ -1,6 +1,31 @@
 use std::io;
 
+const SIZE: uint = 4;
+
+fn pew(level: uint, placed: Vec<bool>, result: Vec<uint>) {
+    if level == SIZE { return println!("{}", result) }
+
+    for a in range(0, SIZE) {
+        if placed[a] { continue }
+
+        let new_level = level + 1;
+        let new_placed = Vec::from_fn(SIZE, |i| i == a || placed[i]);
+        let new_result = Vec::from_fn(new_level, |i| {
+            if i == level { a } else { result[i] }
+        });
+
+        pew(new_level, new_placed, new_result);
+    }
+}
+
 fn main() {
+    let placed = Vec::from_elem(SIZE, false);
+
+    pew(0, placed, Vec::<uint>::new());
+}
+
+#[allow(dead_code)]
+fn print_binary() {
     println!("Rust implements println! as a macro rather than a function");
 
     let input = io::stdin()
