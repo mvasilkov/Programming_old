d932590d4b4028b54523d807d2f35685f9e9e0bb
Mark Vasilkov <mvasilkov@gmail.com>
2014-12-13 21:34:34 +0200

    Changes

--- Rust/src/hello.rs
+++ Rust/src/hello.rs
@@ -1,3 +1,22 @@
+use std::io;
+
 fn main() {
     println!("Rust implements println! as a macro rather than a function");
+
+    let input = io::stdin()
+        .read_line()
+        .ok()
+        .expect("What to do?");
+
+    let input_num: Option<uint> = from_str::<uint>(input.trim());
+
+    let num = match input_num {
+        Some(x) => x,
+        None => {
+            println!("Uh, what?");
+            return;
+        },
+    };
+
+    println!("{:b}", num);
 }
